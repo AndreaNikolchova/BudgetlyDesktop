0d77c10468f51bf228583ce7dc97ea0f761a7862
AndreaNikolchova <106147537+AndreaNikolchova@users.noreply.github.com>
2025-10-07 20:48:15 +0300

    Create BudgetlyContext.cs

---
+++ BudgetlyDesktop/BudgetlyDesktop.Data/BudgetlyContext.cs
@@ -0,0 +1,26 @@
+﻿namespace BudgetlyDesktop.Data
+{
+using Microsoft.EntityFrameworkCore;
+using Models;
+    public class BudgetlyContext : DbContext
+    {
+      
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+            optionsBuilder.UseSqlite("Data Source=BudgetlyDesktop.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+            modelBuilder.Entity<Transaction>()
+                        .HasOne(t => t.Category)
+                        .WithMany(c => c.Transactions)
+                        .HasForeignKey(t => t.CategoryId);
+        }
+    }
+}
