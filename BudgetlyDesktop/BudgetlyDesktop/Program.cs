957c27d143283bdfb7db535cd5b596420ec79cf3
AndreaNikolchova <106147537+AndreaNikolchova@users.noreply.github.com>
2025-10-09 11:11:58 +0300

    adding dependency injection

--- BudgetlyDesktop/BudgetlyDesktop/Program.cs
+++ BudgetlyDesktop/BudgetlyDesktop/Program.cs
@@ -1,6 +1,10 @@
 using BudgetlyDesktop.Data;
+using BudgetlyDesktop.Services.Category;
+using BudgetlyDesktop.Services.Category.Contracts;
 using BudgetlyDesktop.Services.Transaction;
 using BudgetlyDesktop.Services.Transaction.Contracts;
+using BudgetlyDesktop.Services.Type;
+using BudgetlyDesktop.Services.Type.Contracts;
 
 namespace BudgetlyDesktop
 {
@@ -14,9 +18,11 @@ namespace BudgetlyDesktop
          
             var dbContext = new BudgetlyContext();
             ITransactionService transactionService = new TransactionService(dbContext);
+            ICategoryService categoryService = new CategoryService(dbContext);
+            ITypeService typeService = new TypeService(dbContext);
 
            
-            Application.Run(new MainForm(transactionService));
+            Application.Run(new MainForm(transactionService,categoryService,typeService));
         }
     }
 }
