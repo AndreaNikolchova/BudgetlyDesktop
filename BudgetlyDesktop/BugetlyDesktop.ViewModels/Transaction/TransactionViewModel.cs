acbc77d644a0cae5ff5127764b1801097ede198f
AndreaNikolchova <106147537+AndreaNikolchova@users.noreply.github.com>
2025-10-09 11:10:46 +0300

    Create TransactionViewModel.cs

---
+++ BudgetlyDesktop/BugetlyDesktop.ViewModels/Transaction/TransactionViewModel.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BugetlyDesktop.ViewModels.Transaction
+{
+    public class TransactionViewModel
+    {
+        public string Title { get; set; } = null!;
+      
+        public decimal Amount { get; set; }
+      
+        public DateTime Date { get; set; }
+        public string Category { get; set; } = null!;
+        public string Type { get; set; } = null!;
+
+    }
+}
